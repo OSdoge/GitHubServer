7c07be19cc0a5d67c810e025c18275ca63f5d49f
Sun Zizhuo <sunzizhuo33@gmail.com>
2022-10-10 17:32:12 +0800

    naming changes and cleanup

---
+++ GitHubServer.Output/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using GitHubServer;
+
+namespace GitHubServer.Output
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GitHubServer gh = new GitHubServer();
+
+            var s = await gh.GetFile("OSdoge", repoName: "GitHubServer", "LICENSE.txt");
+        }
+    }
+}
---
+++ GitHubServer/GHServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Octokit;
+
+namespace GitHubServer
+{
+    public class GitHubServer
+    {
+        public async Task<string> GetFile(string owner, string repoName, string filePath, string userAgent = "doge")
+        {
+            var url = $"https://api.github.com/repos/{owner}/{repoName}/contents/{filePath}";
+
+            //Get request
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", userAgent);
+
+            var content = await (await client.GetAsync(url)).Content.ReadAsStringAsync();
+
+            //Convert Base64 to 8-Bit Integer Array
+            var jsonList = JObject.Parse(content);
+
+            if (jsonList != null)
+            {
+                var bytes = Convert.FromBase64String(jsonList["content"].ToString());
+                var text = Encoding.UTF8.GetString(bytes);
+
+                return text;
+            }
+
+            throw new Exception("json list is null");
+        }
+    }
+}
