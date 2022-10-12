b4bba84a22605195be7a29787c497150a8036741
OS doge <ngyuheng08@gmail.com>
2022-10-12 20:34:11 +0800

    CreateFile() not working

--- GitHubServer/GHServer.cs
+++ GitHubServer/GHServer.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Octokit;
+using System.Security.Policy;
 
 namespace GitHubServer
 {
@@ -27,13 +28,59 @@ namespace GitHubServer
 
             if (jsonList != null)
             {
-                var bytes = Convert.FromBase64String(jsonList["content"].ToString());
-                var text = Encoding.UTF8.GetString(bytes);
-
+                var text = Encoding.UTF8.GetString(Convert.FromBase64String(jsonList["content"].ToString()));
                 return text;
             }
 
             throw new Exception("json list is null");
+        }
+
+        public async Task<bool[]> CreateFile(string userAgent, string token, string data, string owner, string repoName, string filePath, string message = "N/A", string branch = "main")
+        {
+            var tokenAuth = new Credentials(token);
+            var client = new GitHubClient(new ProductHeaderValue(userAgent));
+            string url = $"https://api.github.com/repos/{owner}/{repoName}/contents/{filePath}";
+            //Authentication
+            client.Credentials = tokenAuth;
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
+
+            bool[] abc;
+            //Creating & Updating File
+            try
+            {
+                //Create CreateFileRequest
+                var createFileRequest = new CreateFileRequest(message, data);
+                //Create File
+                var result = client.Repository.Content.CreateFile(owner, repoName, filePath, createFileRequest);
+                abc = new bool[]  { false, result.IsCanceled, result.IsFaulted, result.IsCompleted };
+            }
+            catch
+            {
+                //Get Sha
+                var content = await (await httpClient.GetAsync(url)).Content.ReadAsStringAsync();
+                var jsonList = JObject.Parse(content);
+                var sha = "";
+                try
+                {
+                    sha = Encoding.UTF8.GetString(Convert.FromBase64String(jsonList["sha"].ToString()));
+                }
+                catch
+                {
+                    throw new Exception("Sha not found.");
+                }
+
+                //Create UpdateFileRequest
+                var updateFileRequest = new UpdateFileRequest(message, data, sha);
+
+                //Update File
+                var result = client.Repository.Content.UpdateFile(owner, repoName, filePath, updateFileRequest);
+                abc = new bool[] { true, result.IsCanceled, result.IsFaulted, result.IsCompleted};
+            }
+
+            return abc;
+
         }
     }
 }
