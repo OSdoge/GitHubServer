f876cdad59ba047318a69bdb872eb02f27e67caa
Sun Zizhuo <sunzizhuo33@gmail.com>
2022-10-13 10:34:29 +0800

    made createfile somewhat work

--- GitHubServer.Output/Program.cs
+++ GitHubServer.Output/Program.cs
@@ -2,6 +2,8 @@
 using System.Text;
 using System.Threading.Tasks;
 using GitHubServer;
+using DotNetEnv;
+using System.Diagnostics;
 
 namespace GitHubServer.Output
 {
@@ -10,9 +12,11 @@ namespace GitHubServer.Output
         static async Task Main(string[] args)
         {
             GitHubServer gh = new GitHubServer();
+            Env.TraversePath().Load();
+            var USERNAME = Env.GetString("USERNAME");
+            var TOKEN = Env.GetString("AUTH_TOKEN");
 
-            var s = await gh.GetFile("OSdoge", "GitHubServer", "LICENSE.txt");
-            Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String(s.Content)));
+            await gh.CreateFile(USERNAME, TOKEN, "among us", USERNAME, "github-api-testing", "t.txt");
         }
     }
 }
---
+++ GitHubServer/CreateFile.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubServer
+{
+    public partial class GitHubServer
+    {
+        public async Task CreateFile(string userAgent, string token, string data, string owner, string repoName, string filePath, string message = "N/A", string branch = "main")
+        {
+            var tokenAuth = new Credentials(token);
+            var client = new GitHubClient(new ProductHeaderValue(userAgent));
+            string url = $"https://api.github.com/repos/{owner}/{repoName}/contents/{filePath}";
+
+            //Authentication
+            client.Credentials = tokenAuth;
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
+
+            var request = new CreateFileRequest(message, data);
+            var result = (await client.Repository.Content.CreateFile(owner, repoName, filePath, request)).Content;
+        }
+    }
+}
